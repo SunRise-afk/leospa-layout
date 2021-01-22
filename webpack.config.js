53f664db690265dcba54c1b9e825b659506e7dca
Illia Ilin <sunrise.th13@gmail.com>
2021-01-22 17:40:39 +0200

    Initializing layout

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/i,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+  },
+};
